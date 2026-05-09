eb020f44632aa61c518d0fd3ba2eda137b8cb4d8
KunDan <kundankd2004@gmail.com>
2026-05-09 16:36:47 +0530

    Adding new methods

--- Day7/Functions.js
+++ Day7/Functions.js
@@ -71,3 +71,14 @@ console.log("\nAddition:", add(10, 5));
 console.log("Subtraction:", subtract(10, 5));
 console.log("Multiplication:", multiply(10, 5));
 console.log("Division:", divide(10, 5));
+
+// Calculator
+
+const calc = {
+    sum: (...op) => op.reduce((acc, curr) => acc + curr, 0),
+    sub: (...op) => op.reduce((acc, curr) => acc - curr, 0),
+    mul: (...op) => op.reduce((acc, curr) => acc * curr, 1),
+    div: (...op) => op.reduce((acc, curr) => acc / curr, 1),
+};
+ 
+console.log(calc.sum(5, 5, 10, 20));
