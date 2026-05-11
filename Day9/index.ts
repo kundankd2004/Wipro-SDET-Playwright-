978b4fe3847ee0faaa2c78ccb035eaf4a280d829
KunDan <kundankd2004@gmail.com>
2026-05-11 12:42:36 +0530

    Basic TS

--- Day9/index.ts
+++ Day9/index.ts
@@ -1,3 +1,11 @@
-const message: string = "Hello TypeScript";
+let name: string = "Aaryan";
+let result: number | string;
+result = 45;
+result = "success";
+console.log(name, result);
 
-console.log(message);
+function add(a: number, b: number): string {
+    return "Sum: " + (a + b);
+}
+
+console.log(add(5, 10));
